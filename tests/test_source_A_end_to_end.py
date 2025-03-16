22bf4c6c7bd9fdf4ea6677b9c1bae433a0899d26
blu300 <bbluestone@outlook.com>
2025-03-16 19:15:08 +0000

    fixed nutter tests

--- tests/test_source_A_end_to_end.py
+++ tests/test_source_A_end_to_end.py
@@ -36,7 +36,7 @@ test_identifier = random.randint(0,1000)
 
 #define variables that are to be used/passed during test
 layers = ["raw", "bronze", "silver", "gold"]
-table_name_dict = {layer: f"{layer}.source_a.{test_identifier}_{layer}_table" 
+table_name_dict = {layer: f"source_a.{test_identifier}_{layer}_table" 
                    for layer in layers}
 
 #construct argument dict to pass to dbutils.notebook.run
@@ -51,33 +51,33 @@ gold_args = {"source_table": table_name_dict["silver"],
 
 # COMMAND ----------
 
-# class MyTestFixture(NutterFixture):
+class MyTestFixture(NutterFixture):
 
-#    def before_source_a_end_to_end(self):
-#       print(test_identifier)
+   def before_source_a_end_to_end(self):
+      print(test_identifier)
 
-#       #create raw data table using test data
-#       test_df.write.saveAsTable(table_name_dict["raw"])
+      #create raw data table using test data
+      test_df.write.saveAsTable(table_name_dict["raw"])
 
-#    def run_source_a_end_to_end(self):
-#       (dbutils.notebook
-#        .run('./source_A_1_raw_to_bronze_notebook', 600, bronze_args))
-#       (dbutils.notebook
-#        .run('./source_A_2_bronze_to_silver_notebook', 600, silver_args))
-#       (dbutils.notebook
-#        .run('./source_A_3_silver_to_gold_notebook', 600, gold_args))
+   def run_source_a_end_to_end(self):
+      (dbutils.notebook
+       .run('../src/source_A_1_raw_to_bronze_notebook', 600, bronze_args))
+      (dbutils.notebook
+       .run('../src/source_A_2_bronze_to_silver_notebook', 600, silver_args))
+      (dbutils.notebook
+       .run('../src/source_A_3_silver_to_gold_notebook', 600, gold_args))
 
-#    def assertion_source_a_end_to_end(self):
-#       output_df = spark.read.table(table_name_dict["gold"])
-#       assertDataFrameEqual(expected_df, output_df)
+   def assertion_source_a_end_to_end(self):
+      output_df = spark.read.table(table_name_dict["gold"])
+      assertDataFrameEqual(expected_df, output_df)
 
-#    def after_source_a_end_to_end(self):
-#       [spark.sql(f"DROP TABLE {table_name_dict[layer]}") for layer in layers]
+   def after_source_a_end_to_end(self):
+      [spark.sql(f"DROP TABLE {table_name_dict[layer]}") for layer in layers]
 
-# result = MyTestFixture().execute_tests()
-# print(result.to_string())
+result = MyTestFixture().execute_tests()
+print(result.to_string())
 # Comment out the next line (result.exit(dbutils)) 
 # to see the test result report from within the notebook
-# result.exit(dbutils)
+result.exit(dbutils)
 
-assert 1==1
+# assert 1==1
