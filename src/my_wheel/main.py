9ea14e8f59d01a03a38cdcb0db1f0e896b562821
blu300 <bbluestone@outlook.com>
2025-03-19 09:16:51 +0000

    test

--- src/my_wheel/main.py
+++ src/my_wheel/main.py
@@ -3,7 +3,7 @@ from my_wheel.my_module import *
 def main():
 
 
-  first = 3103
+  first = 310
   second = 400
   print('hi world')
   print(f"{first} + {second} = {add_two_numbers(first, second)}")
